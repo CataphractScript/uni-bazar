f571d661d1503cab774c6961627aa1a62a7bd42e
CataphractScript <dowlatabadiparsa@gmail.com>
2026-02-25 05:08:12 +0330

    feat(sql): add users table definition to create_tables.sql

---
+++ sql/create_tables.sql
@@ -0,0 +1,12 @@
+-- USER TABLES
+CREATE TABLE users (
+    user_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    fname VARCHAR(100) NOT NULL,
+    lname VARCHAR(100) NOT NULL,
+    phone VARCHAR(20),
+    email VARCHAR(255) UNIQUE,
+    balance BIGINT DEFAULT 0, -- Wallet
+
+    CONSTRAINT check_email_or_phone_not_null
+        CHECK (phone IS NOT NULL OR email IS NOT NULL)
+);
